73d701f08169803fbced7abd4406ef9a94bf0fb0
rider0211 <codemaster9428@gmail.com>
2020-03-22 12:40:17 -0400

    Update README.md

--- src/components/Secure.vue
+++ src/components/Secure.vue
@@ -1,13 +1,9 @@
 <template>
     <div id="secure">
         <h1>Secure Area</h1>
-        <b-container>
-            <b-row>
-                <b-col>Column 1</b-col>
-                <b-col>Column 2</b-col>
-                <b-col>Column 3</b-col>
-            </b-row>
-        </b-container>
+        <p>
+            You "logged in"
+        </p>
     </div>
 </template>
 
