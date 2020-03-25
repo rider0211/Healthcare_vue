d1f0d84d3130dd46357a779be049055a5a395c50
rider0211 <codemaster9428@gmail.com>
2020-03-24 22:11:03 -0400

    fix: Compatability updates for API

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/CfB.png" />
-    <div>{{auth_token}}</div>
-    <div id="nav">
-      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/CfB.png">
+        <div id="nav">
+            <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
+        </div>
+        <router-view @authenticated="setAuthenticated" />
     </div>
-    <router-view @authenticated="setAuthenticated" />
-  </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-
-export default {
-  name: "App",
-  data() {
-    return {
-      authenticated: () =>{
-          if(this.auth_token){
-              return true
-          }else{
-              return false
-          }
-      }
-    };
-  },
-  computed: mapState({
-    auth_token: "auth"
-  }),
-  mounted() {
-    if (!this.authenticated) {
-      this.$router.replace({ name: "login" });
+    export default {
+        name: 'App',
+        data() {
+            return {
+                authenticated: false,
+            }
+        },
+        mounted() {
+            if(!this.authenticated) {
+                this.$router.replace({ name: "login" });
+            }
+        },
+        methods: {
+            setAuthenticated(status) {
+                this.authenticated = status;
+            },
+            logout() {
+                this.authenticated = false;
+            }
+        }
     }
-  },
-  methods: {
-    setAuthenticated(status) {
-      this.authenticated = status;
-    },
-    logout() {
-      this.authenticated = false;
-    }
-  }
-};
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -56,14 +44,14 @@ export default {
 }
 
 body {
-  background-color: #f0f0f0;
-}
-h1 {
-  padding: 0;
-  margin-top: 0;
-}
+        background-color: #F0F0F0;
+    }
+    h1 {
+        padding: 0;
+        margin-top: 0;
+    }
 
-#app img {
+#app img{
   max-width: 25vw;
   min-width: 200px;
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store'
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -11,7 +10,6 @@ Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
   render: h => h(App),
   data(){
     return {
