5c28a77b6299f31ed5821607fe17372c0e0a26f1
rider0211 <codemaster9428@gmail.com>
2020-03-26 23:30:09 -0400

    Help Wanted

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container fluid="md" id="secure">
     <h1>Dashboard</h1>
-    <h3>Hello {{this.$jwt.decode(this.$root.getSavedToken()).email}}</h3>
+    <h3>Hello {{this.$jwt.decode(this.$root.auth_token).email}}</h3>
     <b-row>
       <b-col>
         <!-- Dashboard Table -->
@@ -17,7 +17,11 @@
                 :to="{ name: 'facility', params: { entityID: entity.id }}"
               >{{ entity.name }}</router-link>
             </td>
-            <td>{{ entity.checkIn[0] }}</td>
+            <td>
+              <div v-if="entity.checkin">
+              {{ entity.checkIn.checkIns[0] }}
+              </div>
+              </td>
             <td>{{ entity.updatedAt }}</td>
           </tr>
         </table>
@@ -27,29 +31,26 @@
 </template>
 
 <script>
-
-export default {
-  name: "dashboard",
-  data() {
-    return {
-      entities: null
-    };
-  },
-  methods: {
-    updateEntities(obj) {
-      this.entities = obj;
+  export default {
+    name: "Dashboard",
+    data() {
+      return {
+        entities: null
+      };
+    },
+    methods: {
+      updateEntities(obj) {
+        this.entities = obj.results;
+      }
+    },
+    mounted() {
+      this.$root.apiRequest("/entity", this.updateEntities);
     }
-  },
-  mounted() {
-    this.updateEntities(
-      this.$root.apiRequest("/entity")
-    );
-  }
-};
+  };
 </script>
 
 <style scoped>
-h1 {
-  text-align: left;
-}
+  h1, h3 {
+    text-align: left;
+  }
 </style>
